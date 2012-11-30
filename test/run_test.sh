016c70d8797985e98827210d8889322ac8d8ded6
Peter Levart <Peter.Levart@marand.si>
2012-11-30 17:38:18 +0100

    benchmark results for i7 and new patch against jdk8/tl repo

---
+++ test/run_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$JAVA_HOME" == "" ]; then
+    echo "Please specify JAVA_HOME to point to JDK8 image"
+    exit 1
+fi
+
+if [ -f "$JAVA_HOME/bin/sparcv9/java" ]; then
+    JAVA_BIN="$JAVA_HOME/bin/sparcv9/java"
+else
+    JAVA_BIN="$JAVA_HOME/bin/java"
+fi
+
+if [ "$CP" == "" ]; then
+    CP=../out/production/jdk:../out/production/test
+fi
+
+OPTS="-Xmx4G -cp $CP"
+
+echo ""
+echo "Executing: $JAVA_BIN $OPTS $*"
+echo ""
+$JAVA_BIN $OPTS $*
+
+OPTS="-Xbootclasspath/p:$CP $OPTS"
+
+echo ""
+echo "Executing: $JAVA_BIN $OPTS $*"
+echo ""
+$JAVA_BIN $OPTS $*
