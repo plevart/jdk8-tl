efaee1fc4e4a57c9af3f990deb109ae50e6bdae7
Peter Levart <Peter.Levart@marand.si>
2012-12-14 19:19:49 +0100

    testing sizeOf

--- test/src/test/SizeOfTest.java
+++ test/src/test/SizeOfTest.java
@@ -1,19 +1,54 @@
 package test;
 
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
+ *
+ 536
+ 1016
+ 1064
+ 1272
+ 1472
+ 3232
+
+
  */
 public class SizeOfTest {
-    public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("aaaaa", "11111");
-        map.put("bbbbb", "22222");
-        map.put(new String("ccccc"), new String("33333"));
 
-        SizeOf sizeOf = new SizeOf(SizeOf.Visitor.STDOUT);
-        sizeOf.deepSizeOf(map);
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Ann {}
+
+    class Cls {
+        int f;
+        void m() {}
+    }
+
+    public static void main(String[] args) {
+
+        new SizeOf(SizeOf.Visitor.STDOUT).deepSizeOf(Object.class);
+
+        SizeOf sizeOf = new SizeOf(SizeOf.Visitor.NULL);
+
+        System.out.println(sizeOf.deepSizeOf(Cls.class));
+
+        Cls.class.getDeclaredFields();
+        System.out.println(sizeOf.deepSizeOf(Cls.class));
+
+        Cls.class.getFields();
+        System.out.println(sizeOf.deepSizeOf(Cls.class));
+
+        Cls.class.getDeclaredConstructors();
+        System.out.println(sizeOf.deepSizeOf(Cls.class));
+
+        Cls.class.getDeclaredMethods();
+        System.out.println(sizeOf.deepSizeOf(Cls.class));
+
+        Cls.class.getMethods();
+        System.out.println(sizeOf.deepSizeOf(Cls.class));
     }
 }
