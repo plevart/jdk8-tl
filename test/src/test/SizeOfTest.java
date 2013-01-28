a3812683ebff383760e792dd7b89d6a0c289f961
Peter Levart <Peter.Levart@marand.si>
2013-01-28 17:13:53 +0100

    deeps size of ClassLoader

--- test/src/test/SizeOfTest.java
+++ test/src/test/SizeOfTest.java
@@ -7,7 +7,6 @@ import si.pele.microbench.SizeOf;
 public class SizeOfTest {
     public static void main(String[] args) {
         SizeOf sizeOf = new SizeOf(SizeOf.Visitor.STDOUT);
-        System.out.println(sizeOf.sizeOf(SizeOfTest.class.getClassLoader()));
-        System.out.println(sizeOf.sizeOf(SizeOfTest.class.getClassLoader().getParent()));
+        sizeOf.deepSizeOf(SizeOfTest.class.getClassLoader());
     }
 }
