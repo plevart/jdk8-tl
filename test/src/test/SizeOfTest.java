ba8f6cef8b485a5831955d9065764cd33d3b57b8
Peter Levart <Peter.Levart@marand.si>
2013-01-28 16:26:33 +0100

    add ExtClassLoader to SizeOfTest

--- test/src/test/SizeOfTest.java
+++ test/src/test/SizeOfTest.java
@@ -8,5 +8,6 @@ public class SizeOfTest {
     public static void main(String[] args) {
         SizeOf sizeOf = new SizeOf(SizeOf.Visitor.STDOUT);
         sizeOf.deepSizeOf(SizeOfTest.class.getClassLoader());
+        sizeOf.deepSizeOf(SizeOfTest.class.getClassLoader().getParent());
     }
 }
