a188087e39dd5ce809d0a09f2ed827ebdc717d96
Peter Levart <Peter.Levart@marand.si>
2013-05-14 12:58:36 +0200

    bug reproducer as a test

--- test/src/StringContentEqualsBug.java
+++ test/src/StringContentEqualsBug.java
@@ -1,18 +1,36 @@
 /*
- * Written by Peter Levart <peter.levart@gmail.com>
- * and released to the public domain, as explained at
- * http://creativecommons.org/publicdomain/zero/1.0/
+ * Copyright (c) 2000, 2001, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 
 /**
- * @author peter
+ * @test
+ * @bug 8014477
+ * @summary test String.contentEquals(StringBuffer)
  */
 public class StringContentEqualsBug {
 
     static class Task extends Thread {
         volatile StringBuffer sb;
         volatile Exception exception;
-        int cnt;
 
         Task(StringBuffer sb) {
             this.sb = sb;
@@ -20,14 +38,10 @@ public class StringContentEqualsBug {
 
         @Override
         public void run() {
-            int cnt = 0;
             try {
                 StringBuffer sb;
                 while ((sb = this.sb) != null) {
-                    if ("QQ".contentEquals(sb))
-                        cnt++;
-                    else
-                        cnt--;
+                    "QQ".contentEquals(sb);
                     sb.setLength(0);
                     sb.trimToSize();
                     sb.append("AA");
@@ -36,20 +50,19 @@ public class StringContentEqualsBug {
             catch (Exception e) {
                 exception = e;
             }
-            this.cnt = cnt;
         }
     }
 
     public static void main(String[] args) throws Exception {
         StringBuffer sb = new StringBuffer();
-        Task[] tasks = new Task[2];
+        Task[] tasks = new Task[3];
         for (int i = 0; i < tasks.length; i++) {
             (tasks[i] = new Task(sb)).start();
         }
         try
         {
-            // wait at most 10 seconds for any of the threads to throw exception
-            for (int i = 0; i < 40; i++) {
+            // wait at most 5 seconds for any of the threads to throw exception
+            for (int i = 0; i < 20; i++) {
                 for (Task task : tasks) {
                     if (task.exception != null) {
                         throw task.exception;
