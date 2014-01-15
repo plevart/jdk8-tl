f9fe9fd84eccb6f0443a915d9228c170cfdffdb0
Peter Levart <Peter.Levart@marand.si>
2014-01-15 19:23:48 +0100

    new branch AtomicFieldHandles

--- test/src/IntFieldHandlesTest.java
+++ test/src/IntFieldHandlesTest.java
@@ -4,7 +4,11 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
+import sun.misc.Unsafe;
+
 import java.lang.invoke.IntFieldHandles;
+import java.lang.reflect.Field;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 import static java.lang.invoke.IntFieldHandles.fieldHandles;
 
@@ -12,21 +16,80 @@ import static java.lang.invoke.IntFieldHandles.fieldHandles;
  * @author peter.levart@gmail.com
  */
 public class IntFieldHandlesTest {
-    private int x;
-    private static final IntFieldHandles X = fieldHandles("x");
+    volatile int x;
+
+    static final IntFieldHandles X = fieldHandles("x");
+
+    static final AtomicIntegerFieldUpdater<IntFieldHandlesTest> X_FieldUpdater =
+        AtomicIntegerFieldUpdater.newUpdater(IntFieldHandlesTest.class, "x");
+
+    static final Unsafe U;
+    static final long X_offset;
+
+    static {
+        try {
+            Field U_Field = Unsafe.class.getDeclaredField("theUnsafe");
+            U_Field.setAccessible(true);
+            U = (Unsafe) U_Field.get(null);
+            X_offset = U.objectFieldOffset(IntFieldHandlesTest.class.getDeclaredField("x"));
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
+
+    long javaSet(int n) {
+        long t0 = System.nanoTime();
+        for (int i = 0; i < n; i++) {
+            x = 4321;
+        }
+        return System.nanoTime() - t0;
+    }
+
+    long methodHandleSet(int n) {
+        try {
+            long t0 = System.nanoTime();
+            for (int i = 0; i < n; i++) {
+                X.setRelease.invokeExact(this, 4321);
+            }
+            return System.nanoTime() - t0;
+        } catch (Throwable ignore) { return 0L; }
+    }
+
+    long fieldUpdaterSet(int n) {
+        try {
+            long t0 = System.nanoTime();
+            for (int i = 0; i < n; i++) {
+                X_FieldUpdater.set(this, 4321);
+            }
+            return System.nanoTime() - t0;
+        } catch (Throwable ignore) { return 0L; }
+    }
+
+    long unsafeSet(int n) {
+        try {
+            long t0 = System.nanoTime();
+            for (int i = 0; i < n; i++) {
+                U.putIntVolatile(this, X_offset, 4321);
+            }
+            return System.nanoTime() - t0;
+        } catch (Throwable ignore) { return 0L; }
+    }
 
     public static void main(String[] args) throws Throwable {
+
         IntFieldHandlesTest t = new IntFieldHandlesTest();
-        t.x = 12;
-        System.out.println((int) X.getAcquire.invokeExact(t));
 
-        X.setRelease.invokeExact(t, 13);
-        System.out.println(t.x);
-
-        System.out.println((boolean) X.compareAndSet.invokeExact(t, 13, 14));
-        System.out.println((int) X.get.invokeExact(t));
-
-        System.out.println((boolean) X.compareAndSet.invokeExact(t, 13, 15));
-        System.out.println((int) X.get.invokeExact(t));
+        System.out.println("            Java bytecode: " + t.javaSet(100_000_000));
+        System.out.println("            Java bytecode: " + t.javaSet(100_000_000));
+        System.out.println("            Java bytecode: " + t.javaSet(100_000_000));
+        System.out.println("                   Unsafe: " + t.unsafeSet(100_000_000));
+        System.out.println("                   Unsafe: " + t.unsafeSet(100_000_000));
+        System.out.println("                   Unsafe: " + t.unsafeSet(100_000_000));
+        System.out.println("          IntFieldHandles: " + t.methodHandleSet(100_000_000));
+        System.out.println("          IntFieldHandles: " + t.methodHandleSet(100_000_000));
+        System.out.println("          IntFieldHandles: " + t.methodHandleSet(100_000_000));
+        System.out.println("AtomicIntegerFieldUpdater: " + t.fieldUpdaterSet(100_000_000));
+        System.out.println("AtomicIntegerFieldUpdater: " + t.fieldUpdaterSet(100_000_000));
+        System.out.println("AtomicIntegerFieldUpdater: " + t.fieldUpdaterSet(100_000_000));
     }
 }
