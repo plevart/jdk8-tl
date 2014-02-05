855fb845657d9c5554a52df0a16fdbabd42178b3
Peter Levart <Peter.Levart@marand.si>
2014-02-05 08:20:03 +0100

    fix test (c) year and a bug in printout

--- jdk/src/test-src/DirectBufferAllocTest.java
+++ jdk/src/test-src/DirectBufferAllocTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -107,7 +107,7 @@ public class DirectBufferAllocTest {
 
         System.out.println(
             "Allocating direct ByteBuffers with capacity " +
-            CAPACITY + " bytes, " +
+            capacity + " bytes, " +
             "using " + threads + " threads for " + runTimeSeconds + " seconds..."
         );
 
